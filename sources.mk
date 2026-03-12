ac8464c058ff8dd467607b5df206e914612300ae
Nguyen Khac Thai Khang <khang.nguyen021204@hcmut.edu.vn>
2026-03-12 10:46:08 +0700

    implement sources.mk

--- sources.mk
+++ sources.mk
@@ -10,8 +10,29 @@
 #*****************************************************************************
 
 # Add your Source files to this variable
-SOURCES =
+PLATFORM = MSP432
+
+ifeq($(PLATFORM), MSP432)
+	SOURCES = ./src/main.c \
+			./src/stats.c \
+			./src/platform.c \
+			./src/memory.c \
+			./src/interrupts_msp432p401r_gcc.c \
+			./src/startup_msp432p401r_gcc.c \
+			./src/system_msp432p401r.c
+			
+	INCLUDES = -I ./include/CMSIS \
+			   -I ./include/msp432\
+			   -I ./include/common
+else
+    SOURCES = ./src/main.c \
+			./src/stats.c \
+			./src/platform.c \
+			./src/memory.c \
+	INCLUDES = -I ./include/common
+
+endif
 
 # Add your include paths to this variable
-INCLUDES = 
+
 
